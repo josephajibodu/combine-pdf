36397d537d3cc87a0b9ed4b9108bc669c93af4ee
= <josephajibodu@gmail.com>
2022-05-08 21:24:05 +0100

    ListFiles refactored

--- src/features/ListFiles/ListFiles.tsx
+++ src/features/ListFiles/ListFiles.tsx
@@ -1,12 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import books from '../../app/books.json';
 import BookCard, { Book } from '../../common/components/BookCard';
 
 const ListFiles = () => {
+  const [pdfFiles, setPDFFiles] = useState<Book[]>([]);
+  const [selectedFiles, setSelectedFiles] = useState<Book[]>([]);
+
+  useEffect(() => {
+    setPDFFiles(books);
+  }, [])
+
+  const onSelected = (pdf: Book) => {
+    let files = selectedFiles;
+    files.push(pdf);
+    setSelectedFiles(files);
+
+    console.log(files);
+  }
+
+  const onRemoved = (toBeRemovedPDF: Book) => {
+    let files = selectedFiles.filter((pdf) => toBeRemovedPDF.filename !== pdf.filename);
+    setSelectedFiles(files);
+
+    console.log(files);
+  }
+
   return (
     <div className='grid md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-3 m gap-5'>
-      {books.map((book : Book) => (
-        <BookCard key={book.filename} book={book} />
+      {pdfFiles.map((pdf : Book) => (
+        <BookCard key={pdf.filename} book={pdf} onBookSelected={onSelected} onBookRemoved={onRemoved} />
       ))}
     </div>
   )
