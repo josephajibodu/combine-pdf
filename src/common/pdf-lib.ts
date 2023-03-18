22bc58450e3bc337f3c1087b53ba5428bc78f89a
cremirdevio <josephajibodu@gmail.com>
2023-03-18 09:18:53 -0700

    fix: pdf order of selection maintained
    I had to switch from using promise.all to basic for loop.
    Promise.all behaves differently because some promises
    resolve before the others, resulting in change of order
    in case where the preceeding file is larger than the current.

--- src/common/pdf-lib.ts
+++ src/common/pdf-lib.ts
@@ -18,20 +18,35 @@ export default async function mergePDF(pdfFiles: PDF[]) {
   try {
     const finalPdfDoc = await PDFDocument.create();
 
-    await Promise.all(
-      pdfFiles.map(async (pdf) => {
-        const pdfBytes = await fetch(pdf.source).then((res) => res.arrayBuffer());
+    // This works but, some promise resolve before the others: which then scatters the order of the files.
+    // await Promise.all(
+    //   pdfFiles.map(async (pdf) => {
+    //     const pdfBytes = await fetch(pdf.source).then((res) => res.arrayBuffer());
   
-        const pdfDoc = await PDFDocument.load(pdfBytes);
+    //     const pdfDoc = await PDFDocument.load(pdfBytes);
   
-        const copiedPages = await finalPdfDoc.copyPages(
-          pdfDoc,
-          pdfDoc.getPageIndices()
-        );
+    //     const copiedPages = await finalPdfDoc.copyPages(
+    //       pdfDoc,
+    //       pdfDoc.getPageIndices()
+    //     );
   
-        copiedPages.forEach((page) => finalPdfDoc.addPage(page));
-      })
-    )
+    //     copiedPages.forEach((page) => finalPdfDoc.addPage(page));
+    //   })
+    // )
+
+    for (let index = 0; index < pdfFiles.length; index++) {
+      const pdf = pdfFiles[index];
+      const pdfBytes = await fetch(pdf.source).then((res) => res.arrayBuffer());
+
+      const pdfDoc = await PDFDocument.load(pdfBytes);
+
+      const copiedPages = await finalPdfDoc.copyPages(
+        pdfDoc,
+        pdfDoc.getPageIndices()
+      );
+
+      copiedPages.forEach((page) => finalPdfDoc.addPage(page));
+    }
 
     // It now works!
     const finalPdfBytes = await finalPdfDoc.save();
